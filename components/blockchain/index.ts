3b6c3f5b541543e03f3b5f40792cebf0dca17f3f
Emeka <Kingsleyokoli71@gmail.com>
2026-02-24 09:58:16 +0100

    engagement side bar

---
+++ components/blockchain/index.ts
@@ -0,0 +1,17 @@
+// Blockchain Components
+export { RewardsConfig } from './RewardsConfig';
+export { RewardClaimModal } from './RewardClaimModal';
+export { RewardsDemo } from './RewardsDemo';
+
+// Types
+export type {
+  RewardRule,
+  EligibilityCriteria,
+  RewardPool,
+  RewardConfig,
+  UserReward,
+  ClaimStatus,
+} from '../../blockchain/types/rewards';
+
+// Services
+export { RewardsService } from '../../blockchain/services/RewardsService';
